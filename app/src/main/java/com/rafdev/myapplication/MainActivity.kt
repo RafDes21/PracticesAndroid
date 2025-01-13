aa5a6efa2a840a5f0c6a72d6c220917aee6a1cd3
israe <israeltineo84@gmail.com>
2025-01-13 15:11:10 -0300

    create animUtils

--- app/src/main/java/com/rafdev/myapplication/MainActivity.kt
+++ app/src/main/java/com/rafdev/myapplication/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.core.animation.doOnEnd
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.rafdev.myapplication.utils.AnimUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,44 +33,47 @@ class MainActivity : AppCompatActivity() {
         val btnCancel = findViewById<Button>(R.id.btn_cancel)
 
         btnPlay.setOnClickListener {
-            showButtonWithValueAnimator(container, progressBtn)
+            val screenWidth = resources.displayMetrics.widthPixels
+            AnimUtils.slideInFromRight(container, screenWidth) {
+                AnimUtils.animateProgressBar(progressBtn)
+            }
         }
 
     }
 
-    private fun showButtonWithValueAnimator(container: View, progressBar: ProgressBar) {
-        val screenWidth = resources.displayMetrics.widthPixels
-
-        val animator = ValueAnimator.ofFloat(screenWidth.toFloat(), 0f).apply {
-            duration = 700
-            interpolator = DecelerateInterpolator()
-            addUpdateListener { valueAnimator ->
-                val value = valueAnimator.animatedValue as Float
-                container.translationX = value
-            }
-            doOnEnd {
-                container.translationX = 0f
-                animateBackgroundExpansion(progressBar)
-            }
-        }
-        container.visibility = View.VISIBLE
-        animator.start()
-    }
-
-    private fun animateBackgroundExpansion( progressBar: ProgressBar) {
-        progressBar.visibility = View.VISIBLE
-        progressBar.progress = 0
-
-        val animator = ValueAnimator.ofInt(0, 100).apply {
-            duration = 4000
-            interpolator = LinearInterpolator()
-            addUpdateListener { valueAnimator ->
-                val progress = valueAnimator.animatedValue as Int
-                progressBar.progress = progress
-            }
-        }
-
-        animator.start()
-    }
+//    private fun showButtonWithValueAnimator(container: View, progressBar: ProgressBar) {
+//        val screenWidth = resources.displayMetrics.widthPixels
+//
+//        val animator = ValueAnimator.ofFloat(screenWidth.toFloat(), 0f).apply {
+//            duration = 700
+//            interpolator = DecelerateInterpolator()
+//            addUpdateListener { valueAnimator ->
+//                val value = valueAnimator.animatedValue as Float
+//                container.translationX = value
+//            }
+//            doOnEnd {
+//                container.translationX = 0f
+//                animateBackgroundExpansion(progressBar)
+//            }
+//        }
+//        container.visibility = View.VISIBLE
+//        animator.start()
+//    }
+//
+//    private fun animateBackgroundExpansion( progressBar: ProgressBar) {
+//        progressBar.visibility = View.VISIBLE
+//        progressBar.progress = 0
+//
+//        val animator = ValueAnimator.ofInt(0, 100).apply {
+//            duration = 4000
+//            interpolator = LinearInterpolator()
+//            addUpdateListener { valueAnimator ->
+//                val progress = valueAnimator.animatedValue as Int
+//                progressBar.progress = progress
+//            }
+//        }
+//
+//        animator.start()
+//    }
 
 }
---
+++ app/src/main/java/com/rafdev/myapplication/utils/AnimUtils.kt
@@ -0,0 +1,43 @@
+package com.rafdev.myapplication.utils
+
+import android.animation.ValueAnimator
+import android.view.View
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.LinearInterpolator
+import android.widget.ProgressBar
+import androidx.core.animation.doOnEnd
+
+object AnimUtils {
+
+    fun slideInFromRight(container: View, screenWidth: Int, onEnd: () -> Unit = {}) {
+        val animator = ValueAnimator.ofFloat(screenWidth.toFloat(), 0f).apply {
+            duration = 700
+            interpolator = DecelerateInterpolator()
+            addUpdateListener { valueAnimator ->
+                val value = valueAnimator.animatedValue as Float
+                container.translationX = value
+            }
+            doOnEnd {
+                container.translationX = 0f
+                onEnd()
+            }
+        }
+        container.visibility = View.VISIBLE
+        animator.start()
+    }
+
+    fun animateProgressBar(progressBar: ProgressBar, duration: Long = 4000) {
+        progressBar.visibility = View.VISIBLE
+        progressBar.progress = 0
+
+        val animator = ValueAnimator.ofInt(0, 100).apply {
+            this.duration = duration
+            interpolator = LinearInterpolator()
+            addUpdateListener { valueAnimator ->
+                val progress = valueAnimator.animatedValue as Int
+                progressBar.progress = progress
+            }
+        }
+        animator.start()
+    }
+}
