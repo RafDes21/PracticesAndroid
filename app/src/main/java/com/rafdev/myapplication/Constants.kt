dc3e6c6397870dc3b7cacdcabab18398516d9f59
israe <israeltineo84@gmail.com>
2025-01-15 19:22:33 -0300

    add

---
+++ app/src/main/java/com/rafdev/myapplication/Constants.kt
@@ -0,0 +1,19 @@
+package com.rafdev.myapplication
+
+
+object Constants{
+    const val APP_VERSION = "app-name"
+
+}
+
+object CalculatorAction{
+    const val ALL_CLEAR = "AC"
+    const val BACKSPACE = "backspace"
+    const val EQUALS = "="
+}
+
+object CalculatorErrors {
+    const val SYNTAX_ERROR = "Error de sintaxis"
+    const val ARITHMETIC_ERROR = "Error aritmético"
+    const val UNKNOWN_ERROR = "Error desconocido"
+}
